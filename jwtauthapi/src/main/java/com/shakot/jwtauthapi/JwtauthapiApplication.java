345966348854334305d8d1fd1fe2c883feebd610
Shakot  Gabriel <shakotgabriel1@gmail.com>
2026-03-13 21:50:47 +0300

    Initial commit

---
+++ jwtauthapi/src/main/java/com/shakot/jwtauthapi/JwtauthapiApplication.java
@@ -0,0 +1,13 @@
+package com.shakot.jwtauthapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtauthapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtauthapiApplication.class, args);
+	}
+
+}
---
+++ jwtauthapi/src/test/java/com/shakot/jwtauthapi/JwtauthapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shakot.jwtauthapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtauthapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
